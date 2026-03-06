9f29c3b0c94ac6765e8eb38518def7dcb40ae2f2
Salvador <stemas@miumg.edu.gt>
2026-03-05 16:48:27 -0800

    Primer commit: proyecto limpio

--- src/ejercicios/Ejerciciosderepasoparcial1java.java
+++ src/ejercicios/Ejerciciosderepasoparcial1java.java
@@ -14,7 +14,7 @@ public class Ejerciciosderepasoparcial1java {
             System.out.println("   MENÚ DE EJERCICIOS 1-20");
             System.out.println("========================================");
             System.out.println("1. Suma de divisibles entre 4 (1 a n)");
-            System.out.println("2. Factorial de n (!n)");
+            
             System.out.println("0. Salir");
             System.out.print("\nElija una opción: ");
             
@@ -22,7 +22,7 @@ public class Ejerciciosderepasoparcial1java {
 
             switch (opcion) {
                 case 1: sumarDivisibles4(sc); break;
-                case 2: calcularFactorial(sc); break;
+               
                 case 0: System.out.println("Saliendo... ¡Buen trabajo!"); break;
                 
                 default: System.out.println("Opción inválida.");
@@ -43,15 +43,5 @@ public class Ejerciciosderepasoparcial1java {
             if (i % 4 == 0) suma += i;
         }
         System.out.println("La suma de los divisibles entre 4 es: " + suma);
-    }
-  //EJERCICIO 2
-    public static void calcularFactorial(Scanner sc) {
-        System.out.print("Ingrese n para calcular su factorial: ");
-        int n = sc.nextInt();
-        long factorial = 1; 
-        for (int i = 1; i <= n; i++) {
-            factorial *= i;
-        }
-        System.out.println("El factorial de " + n + " es: " + factorial);
     }
 }
